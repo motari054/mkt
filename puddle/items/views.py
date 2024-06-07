58952521e17d768175d7f87926f9ea30d7789fe1
motarisam <samuelmotari54@gmail.com>
2024-06-07 16:24:15 +0300

    changes in detail.html

--- puddle/items/views.py
+++ puddle/items/views.py
@@ -3,7 +3,9 @@ from .models import Items
 
 def detail(request, pk):
     items = get_object_or_404(Items, pk=pk)
+    related_items = Items.objects.filter(category=items.category, is_sold=False).exclude(pk=pk)[0:3]
 
     return render(request, 'items/detail.html', {
         'items':items,
+        'related_items':related_items,
     })
