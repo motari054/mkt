3fffcc6a350d01978edc2f272c9e06f08ae2ee41
motarisam <samuelmotari54@gmail.com>
2024-06-07 14:30:31 +0300

    items/views corrected

--- puddle/items/views.py
+++ puddle/items/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from .models import Items
 
 def detail(request, pk):
-    items = get_object_or_404(items, pk=pk)
+    items = get_object_or_404(Items, pk=pk)
 
     return render(request, 'items/detail.html', {
         'items':items,
