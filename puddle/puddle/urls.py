79dcdb1bc5e948b03977076aec9d3ce6f378b159
motarisam <samuelmotari54@gmail.com>
2024-06-07 16:41:02 +0300

    core.urls updated

--- puddle/puddle/urls.py
+++ puddle/puddle/urls.py
@@ -2,11 +2,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-from core.views import index, contact
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index, name='index'),
+    path('', include('core.urls')),
     path('items', include('items.urls')),
-    path('contact/', contact, name='contact')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
